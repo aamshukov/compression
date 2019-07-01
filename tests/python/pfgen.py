4f9b7387a98b3c806593bd2ce94d5982625f93e1
Arthur <aamshukov@gmail.com>
2019-06-30 20:48:49 -0400

    pfgen fix for crappybuggy microsoft error on sys.exit(0).

--- tests/python/pfgen.py
+++ tests/python/pfgen.py
@@ -1,2 +1,26 @@
 import os
 import sys
+
+from random import randint
+
+def generate():
+    pairs = [ [1,2], [1,3], [1,4], [2,1], [2,3], [2,4], [3,1], [3,2], [3,4], [4,1], [4,2], [4,3] ]
+    with open(r'd:\tmp\pf.gen.txt', 'w') as stream:
+        for k in range(1, 16 * 1024):
+            pair = pairs[randint(0, 11)]
+            stream.write(str(pair[0]) + str(pair[1]) + ' ')
+            if k % 32 == 0:
+                stream.write('\n')
+
+
+def main(args):
+    try:
+        generate()
+    except Exception as ex:
+        print(ex)
+    return 1
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    # buggy microft cannot fix the crap --- sys.exit(main(sys.argv[1:]))
